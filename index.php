2f17f072a3bd0211e439e604b5c73cb1af9c5cab
Omar Cama Huarahuara <omar.js2023@gmail.com>
2020-11-13 09:56:59 -0300

    Reestruturado
    Rondando

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Full Stack Eletro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    
+</head>
+<body>
+    <!--Inicio do menu-->
+    <?php
+        include('menu.html');
+    ?>
+    <!--Fin de Menu-->
+
+    
+    <br><br><br>
+
+
+    <!--Inicio do Corpo-->
+    <main class="container">
+        <h1>Seja bem vindo(a)!</h1>
+    <p>Aqui nosa loja, <em>programadores tem desconto</em> nos productos para sua casa!</p>
+    <hr>
+    </main>
+    <!--Fin do Corpo-->
+
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    
+    <!--Inicio do Rodape-->
+    <footer id="rodape">
+        <p id="formasPagamento"><b>Formas de pagamento</b></p>
+        <img src="./img/usapagos.png" alt="Formas de pagamento">
+        <p>&copy Recode Pro</p>
+    </footer>
+    <!--Fin de Rodape-->
+</body>
+</html>
---
+++ loja.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Nossas Lojas - Full Stack Electro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <!--Inicio do menu-->
+    <?php
+        include('menu.html');
+    ?>
+    <!--Fin de Menu-->
+
+    
+    <!--Inicio do Corpo-->
+    <div class="container">
+    <header>
+        <h2>Nossas Lojas!</h2>
+    </header>
+    <hr>
+    <section class="container filiais">
+        <div class="filiais-box">
+            <h3>Rio de Janeiro</h3>
+                <p>Avenida Presidente Varga, 5000</p>
+                <p>10 &ordm : andar</p>
+                <p>Centro</p>
+                <p>(21) 3333-3333</p>
+        </div>
+        <div class="filiais-box">
+            <h3>Sao Paulo</h3>
+                <p>Avenida Paulista, 6000</p>
+                <p>10 &ordm : andar</p>
+                <p>Centro</p>
+                <p>(21) 4444-4443</p>
+        </div>
+        <div class="filiais-box">
+            <h3>Santa Catalina</h3>
+                <p>Avenida Presidente Varga, 5000</p>
+                <p>10 &ordm : andar</p>
+                <p>Centro</p>
+                <p>(21) 3333-3333</p>
+        </div>
+    </section>
+    </div>
+    
+    <!--Fin do Corpo-->
+
+    <br><br><br><br><br>
+    <br><br><br><br><br>
+    
+    <!--Inicio do Rodape-->
+    <footer id="rodape">
+        <p id="formasPagamento"><b>Formas de pagamento</b></p>
+        <img src="./img/usapagos.png" alt="Formas de pagamento">
+        <p>&copy Recode Pro</p>
+    </footer>
+    <!--Fin do Rodape-->
+</body>
+</html>
